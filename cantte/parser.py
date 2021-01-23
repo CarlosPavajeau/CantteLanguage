c4c95cb2bf2c215ac6b6b008153013a5d88d1498
Carlos Pavajeau <cantte098@gmail.com>
2021-01-23 14:06:08 -0500

    feat: include the generation of errors in the parser!

--- cantte/parser.py
+++ cantte/parser.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 
 from cantte.token import Token, TokenType
 from cantte.lexer import Lexer
@@ -11,9 +11,14 @@ class Parser:
         self._lexer = lexer
         self._current_token: Optional[Token] = None
         self._peek_token: Optional[Token] = None
+        self._errors: List[str] = []
 
         self._advance_tokens()
         self._advance_tokens()
+
+    @property
+    def errors(self) -> List[str]:
+        return self._errors
 
     def parse_program(self) -> Program:
         program: Program = Program(statements=[])
@@ -49,7 +54,16 @@ class Parser:
             self._advance_tokens()
 
             return True
+
+        self._expected_token_error(token_type)
         return False
+
+    def _expected_token_error(self, token_type: TokenType) -> None:
+        assert self._peek_token is not None
+        error = f'The following token \'{self._peek_token.token_type}\' was not expected. ' \
+                f'Was expected \'{token_type}\'.'
+
+        self._errors.append(error)
 
     def _parse_let_statement(self) -> Optional[LetStatement]:
         assert self._current_token is not None
